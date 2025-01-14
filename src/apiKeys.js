f9e40eb42aa60763cd1131cf2077b96378fb6154
Rambha <rambhaverma944@gmail.com>
2025-01-14 17:50:58 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "af433895f62f913b4a70e1a34c736872",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
